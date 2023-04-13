f8c1b91681fb211b537399efbc7d3a47cd8170cc
saifsammy <saifsammy653@gmail.com>
2023-04-13 07:04:55 +0600

    statistics page successfully done

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,78 +1,32 @@
-import React from 'react';
-import {
-    ComposedChart,
-    Line,
-    Area,
-    Bar,
-    XAxis,
-    YAxis,
-    CartesianGrid,
-    Tooltip,
-    Legend,
-    ResponsiveContainer,
-} from 'recharts';
+import React, { PureComponent } from 'react';
+import { ResponsiveContainer, PieChart, Pie, Legend } from 'recharts';
+
 const data = [
-    {
-        name: 'Page A',
-        uv: 590,
-        pv: 800,
-        amt: 1400,
-    },
-    {
-        name: 'Page B',
-        uv: 868,
-        pv: 967,
-        amt: 1506,
-    },
-    {
-        name: 'Page C',
-        uv: 1397,
-        pv: 1098,
-        amt: 989,
-    },
-    {
-        name: 'Page D',
-        uv: 1480,
-        pv: 1200,
-        amt: 1228,
-    },
-    {
-        name: 'Page E',
-        uv: 1520,
-        pv: 1108,
-        amt: 1100,
-    },
-    {
-        name: 'Page F',
-        uv: 1400,
-        pv: 680,
-        amt: 1700,
-    },
+    { name: 'Assignment 1', value: 60 },
+    { name: 'Assignment 2', value: 60 },
+    { name: 'Assignment 3', value: 60 },
+    { name: 'Assignment 4', value: 60 },
+    { name: 'Assignment 5', value: 50 },
+    { name: 'Assignment 6', value: 60 },
+    { name: 'Assignment 7', value: 60 },
+    { name: 'Assignment 8', value: 60 },
+
 ];
+
+
 const Statistics = () => {
     return (
-        <ResponsiveContainer width="100%" height="100%">
-            <ComposedChart
-                width={500}
-                height={400}
-                data={data}
-                margin={{
-                    top: 20,
-                    right: 80,
-                    bottom: 20,
-                    left: 20,
-                }}
-            >
-                <CartesianGrid stroke="#f5f5f5" />
-                <XAxis dataKey="name" label={{ value: 'Pages', position: 'insideBottomRight', offset: 0 }} scale="band" />
-                <YAxis label={{ value: 'Index', angle: -90, position: 'insideLeft' }} />
-                <Tooltip />
-                <Legend />
-                <Area type="monotone" dataKey="amt" fill="#8884d8" stroke="#8884d8" />
-                <Bar dataKey="pv" barSize={20} fill="#413ea0" />
-                <Line type="monotone" dataKey="uv" stroke="#ff7300" />
-            </ComposedChart>
-        </ResponsiveContainer>
+        <div className='text-center'>
+            <h2 className='text-2xl my-5'>Statistics</h2>
+            <h3 className='text-3xl font-bold my-5'>Avarage Marks: 58.75</h3>
+            <div style={{ width: '100%', height: 300 }}>
+                <ResponsiveContainer>
+                    <PieChart>
+                        <Pie dataKey="value" data={data} fill="#8884d8" label />
+                    </PieChart>
+                </ResponsiveContainer>
+            </div>
+        </div>
     );
 };
 
